87f22668783a94f00dbeffc2b696519bc1600b2a
Celia Lobet <lobet.c@gmail.com>
2021-04-23 07:39:12 +0200

    another commit

--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,10 +1,16 @@
-require_relative '../lib/dark_trader.rb'
+require_relative '../lib/crypto.rb'
+require 'nokogiri'
 
-# describe "crypto_scrapper method" do
-  describe "Pas d'erreurs ni de retour vide" do
-    it "should" do
-      # expect(crypo_scrapper).not_to be_nil
-      expect(crypto_scrapper).not_to be_empty
+describe "crypto_scrapper method" do
+
+  describe "fonctionnement de base ok" do
+    it "vérifier si pas de nil" do
+      expect(crypto_scrapper).not_to be_nil
+    end
+
+    it "vérifier taille du tableau" do
+      expect(crypto_scrapper.size).to eq(200)
     end
   end
-# end
+
+end
