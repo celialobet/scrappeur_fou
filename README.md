e6d0a8ec62243514c51277f204093e08bab5d549
Celia Lobet <lobet.c@gmail.com>
2021-04-23 07:34:00 +0200

    fourth commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Projet THP jour 14 - Scrappeur fou
+Scraping avec Ruby (automatisation la récupération massive de données issus de sites internet)
+Utilisation de la gem Nokogiri
+
+1er exercice : programme qui récupère le cours de toutes les cryptomonnaies et les enregistre bien proprement dans un array de hashs
+
+2eme exercice : Récupération des adresses e-mails des mairies du Val d'Oise
+
+3eme exercice : Récupération la liste complète des députés de France ainsi que leurs adresses e-mail
--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -2,32 +2,110 @@ require 'nokogiri'
 require 'open-uri'
 
 
+def get_townhall_urls 
 
-def get_townhall_urls
-  page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
-  townhall_urls_list = []
-  townhall_urls = page.xpath('//td[1]/p/a/@href | //td[2]/p/a/@href | //td[3]/p/a/@href')
-  townhall_urls.each do |node|
-    townhall_urls_list << node
+  annuaire = []
+  page = Nokogiri::HTML(URI.open("https://www.annuaire-des-mairies.com/val-d-oise.html"))
+
+  page.xpath('//a[@class="lientxt"]/@href').each do |url| 
+    annuaire << "https://www.annuaire-des-mairies.com/#{url.text}"
   end
-end
 
-def get_townhall_names
-  page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
-  townhall_names_list = []
-  townhall_names = page.xpath('//td[1]/p/a[@href] | //td[2]/p/a[@href] | //td[3]/p/a[@href]')
-  townhall_names.each do |node|
-    townhall_names_list << node.text
+
+  def get_townhall_email(townhall_url)
+
+    page = Nokogiri::HTML(URI.open("#{townhall_url}"))
+    puts page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+    
   end
-end
 
-def get_townhall_email(townhall_url)
-  page = Nokogiri::HTML(URI.open("https://www.annuaire-des-mairies.com/95/avernes.html"))
-  townhall_email = page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]')
-  townhall_email = townhall_email.text
-end
-
-
-def perform
+  i = 0
+  while annuaire[i] != nil do
+    get_townhall_email("#{annuaire[i]}")
+    i+=1
+  end
 
 end
+
+print get_townhall_urls
+
+
+
+
+
+#Autres essais
+
+
+
+# def get_townhall_urls
+#   page = Nokogiri::HTML(URI.open(townhall_url))
+#   townhall_urls_list = []
+#   page.xpath('//a[@class="lientxt"]/@href').each do |url| 
+#     townhall_urls_list << "https://www.annuaire-des-mairies.com/#{url.text}"
+#     end
+#   return townhall_urls_list
+# end
+# # get_townhall_urls
+
+# def get_townhall_names
+#   page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
+#   townhall_names_list = []
+#   townhall_names = page.xpath('//td[1]/p/a[@href] | //td[2]/p/a[@href] | //td[3]/p/a[@href]')
+#   townhall_names.each do |node|
+#     townhall_names_list << node.text
+#   end
+#   return townhall_names_list
+# end
+# # get_townhall_names
+
+#  def get_townhall_email(townhall_url)
+#     page = Nokogiri::HTML(URI.open(townhall_url))
+#     return page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+#  end
+
+#     email_addresses = []
+#     i = 0
+#     while email_address_path[i] != nil do
+#     email_addresses"#{email_address_path [i]}"
+#     i += 1
+#   end
+#   puts email_addresses
+#   end
+  
+
+# def perform(url)
+#   get_townhall_urls(url).each do |url|
+#   get_townhall_email(url)
+#   end
+# end
+# puts perform("http://annuaire-des-mairies.com/val-d-oise.html")
+
+
+# def get_townhall_urls
+#   page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
+#   townhall_urls_list = []
+
+
+
+    
+     
+
+
+
+# puts get_townhall_urls
+
+
+# def get_townhall_names
+#   page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
+#   townhall_names_list = []
+#   townhall_names = page.xpath('//td[1]/p/a[@href] | //td[2]/p/a[@href] | //td[3]/p/a[@href]')
+#   townhall_names.each do |node|
+#     townhall_names_list << node.text
+#     puts townhall_names_list
+#   end
+# end
+
+
+# def perform(url)
+
+# end
